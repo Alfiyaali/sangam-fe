89009da9bcc1003fe721cf3c504a976f0be2e509
Abhishek Gajera <abhishek.gajera@appsontechnologies.in>
2024-06-07 12:50:46 +0530

    fix: code

--- src/shared/components/Footer/Footer.js
+++ src/shared/components/Footer/Footer.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './Footer.css';
-import Logo from '../../../assets/images/footer-logo.png';
+import React from "react";
+import "./Footer.css";
+import Logo from "../../../assets/images/footer-logo.png";
 
 const Footer = () => {
   return (
@@ -18,11 +18,17 @@ const Footer = () => {
           <div className="footer-address-container">
             <div className="footer-address">
               <h4>India</h4>
-              <p>F-160, First Floor, Corporate Wing, Fantasia Business Park, sector 30B, Vashi Navi Mumbai, Maharashtra 400703</p>
+              <p>
+                F-160, First Floor, Corporate Wing, Fantasia Business Park,
+                sector 30B, Vashi Navi Mumbai, Maharashtra 400703
+              </p>
             </div>
             <div className="footer-address">
               <h4>Registered Office</h4>
-              <p>Suite 12, 32-33 Elmwood Ave, Co. Antrim, Belfast, BT96AZ, United Kingdom</p>
+              <p>
+                Suite 12, 32-33 Elmwood Ave, Co. Antrim, Belfast, BT96AZ, United
+                Kingdom
+              </p>
             </div>
           </div>
         </div>
@@ -30,26 +36,46 @@ const Footer = () => {
           <div className="footer-menu">
             <h3>Privacy & You</h3>
             <ul>
-              <li><a href="/terms">Terms of Use</a></li>
-              <li><a href="privacy">Privacy Policy</a></li>
-              <li><a href="online">Be Safe Online</a></li>
-              <li><a href="report">Report Misuse</a></li>
+              <li>
+                <a href="/terms">Terms of Use</a>
+              </li>
+              <li>
+                <a href="privacy">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="online">Be Safe Online</a>
+              </li>
+              <li>
+                <a href="report">Report Misuse</a>
+              </li>
             </ul>
           </div>
           <div className="footer-menu">
             <h3>Need Help</h3>
             <ul>
-              <li><a href="/login">Member Login</a></li>
-              <li><a href="/register">Sign Up</a></li>
-              <li><a href="/search">Partner Search</a></li>
-              <li><a href="Premium">Premium Membership</a></li>
-              <li><a href="/customer-support">Customer Support</a></li>
+              <li>
+                <a href="/login">Member Login</a>
+              </li>
+              <li>
+                <a href="/register">Sign Up</a>
+              </li>
+              <li>
+                <a href="/search">Partner Search</a>
+              </li>
+              <li>
+                <a href="career">Career</a>
+              </li>
+              <li>
+                <a href="/customer-support">Customer Support</a>
+              </li>
             </ul>
           </div>
           <div className="footer-menu">
             <h3>More</h3>
             <ul>
-              <li><a href="/story">Success Stories</a></li>
+              <li>
+                <a href="/story">Success Stories</a>
+              </li>
             </ul>
           </div>
         </div>
